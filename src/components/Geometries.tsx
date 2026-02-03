c2ad9c59642454c84e697572f8acb8bbeb85f628
hieunv <hieuls63@gmail.com>
2026-02-04 03:07:12 +0700

    update light

--- src/components/Geometries.tsx
+++ src/components/Geometries.tsx
@@ -15,32 +15,69 @@ const Geometries = () => {
   const mainRenderTarget = useFBO();
 
   const {
+    light,
+    shininess,
+    diffuseness,
     iorR,
+    iorY,
     iorG,
+    iorC,
     iorB,
+    iorP,
     saturation,
     chromaticAberration,
-    refractPower,
+    refraction
   } = useControls({
+    light: {
+      x: -1.0,
+      y: 1.0,
+      z: 1.0,
+    },
+    diffuseness: {
+      value: 0.2,
+    },
+    shininess: {
+      value: 40.0,
+    },
     ior: folder({
-      title: 'IOR',
-      iorR: { value: 1.15, min: 1.0, max: 2.333, step: 0.001 },
-      iorG: { value: 1.18, min: 1.0, max: 2.333, step: 0.001 },
-      iorB: { value: 1.22, min: 1.0, max: 2.333, step: 0.001 },
+      iorR: { min: 1.0, max: 2.333, step: 0.001, value: 1.15 },
+      iorY: { min: 1.0, max: 2.333, step: 0.001, value: 1.16 },
+      iorG: { min: 1.0, max: 2.333, step: 0.001, value: 1.18 },
+      iorC: { min: 1.0, max: 2.333, step: 0.001, value: 1.22 },
+      iorB: { min: 1.0, max: 2.333, step: 0.001, value: 1.22 },
+      iorP: { min: 1.0, max: 2.333, step: 0.001, value: 1.22 },
     }),
-    saturation: { value: 0.5, min: 0.0, max: 1.0, step: 0.01 },
-    chromaticAberration: { value: 0.5, min: 0.0, max: 1.5, step: 0.01 },
-    refractPower: { value: 1.06, min: 1, max: 1.25, step: 0.01 },
-  });
+    saturation: { value: 1.08, min: 1, max: 1.25, step: 0.01 },
+    chromaticAberration: {
+      value: 0.6,
+      min: 0,
+      max: 1.5,
+      step: 0.01,
+    },
+    refraction: {
+      value: 0.4,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+  })
 
   const uniforms = useMemo(() => ({
     uTexture: { value: null },
     uIorR: { value: 1.0 },
     uIorG: { value: 1.0 },
     uIorB: { value: 1.0 },
+    uIorY: { value: 1.0 },
+    uIorC: { value: 1.0 },
+    uIorP: { value: 1.0 },
     uSaturation: { value: 0.0 },
     uChromaticAberration: { value: 1.0 },
     uRefractPower: { value: 0.2 },
+    uShininess: { value: 40.0 },
+    uDiffuseness: { value: 0.2 },
+    uLight: {
+      value: new THREE.Vector3(-1.0, 1.0, 1.0),
+    },
     winResolution: {
       value: new THREE.Vector2(
         window.innerWidth,
@@ -61,13 +98,24 @@ const Geometries = () => {
     gl.setRenderTarget(null);
     meshRef.current.visible = true;
 
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uDiffuseness.value = diffuseness;
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uShininess.value = shininess;
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uLight.value = new THREE.Vector3(
+      light.x,
+      light.y,
+      light.z
+    );
+
     (meshRef.current.material as THREE.ShaderMaterial).uniforms.uIorR.value = iorR;
     (meshRef.current.material as THREE.ShaderMaterial).uniforms.uIorG.value = iorG;
     (meshRef.current.material as THREE.ShaderMaterial).uniforms.uIorB.value = iorB;
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uIorY.value = iorY;
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uIorC.value = iorC;
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uIorP.value = iorP;
 
     (meshRef.current.material as THREE.ShaderMaterial).uniforms.uSaturation.value = saturation;
     (meshRef.current.material as THREE.ShaderMaterial).uniforms.uChromaticAberration.value = chromaticAberration;
-    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uRefractPower.value = refractPower;
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uRefractPower.value = refraction;
 
   });
 
@@ -92,7 +140,7 @@ const Geometries = () => {
         )}
       </group>
       <mesh ref={meshRef}>
-        <icosahedronGeometry args={[2.84, 20]} />
+        <torusGeometry args={[3, 1, 32, 100]} />
         <shaderMaterial
           vertexShader={vertexShader}
           fragmentShader={fragmentShader}
