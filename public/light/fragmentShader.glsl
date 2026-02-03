2a50bd3956b4b17392def697624b95d38e1d4358
hieunv <hieuls369@gmail.com>
2026-02-03 16:21:33 +0700

    update refract

--- public/light/fragmentShader.glsl
+++ public/light/fragmentShader.glsl
@@ -1,11 +1,52 @@
+uniform float uIorR;
+uniform float uIorG;
+uniform float uIorB;
+uniform float uSaturation;
+uniform float uChromaticAberration;
+uniform float uRefractPower;
 uniform vec2 winResolution;
 uniform sampler2D uTexture;
 
+varying vec3 worldNormal;
+varying vec3 eyeVector;
+
+const int LOOP = 16;
+
+vec3 sat(vec3 rgb, float intensity) {
+  vec3 L = vec3(0.2125, 0.7154, 0.0721);
+  vec3 grayscale = vec3(dot(rgb, L));
+  return mix(grayscale, rgb, intensity);
+
+}
+
+
 void main() {
-  vec2 uv = gl_FragCoord.xy / winResolution.xy;
-  vec4 color = texture2D(uTexture, uv);
+  float iorRatioR = 1.0/uIorR;
+  float iorRatioG = 1.0/uIorG;
+  float iorRatioB = 1.0/uIorB;
   
-  gl_FragColor = color;
+  vec3 color = vec3(1.0);
+
+  vec2 uv = gl_FragCoord.xy / winResolution.xy;
+  vec3 normal = worldNormal;
+
+  for (int i = 0; i < LOOP; i++) {
+    float slide = float(i) / float(LOOP) * 0.1;
+
+    vec3 refractVecR = refract(eyeVector, normal, iorRatioR);
+    vec3 refractVecG = refract(eyeVector, normal, iorRatioG);
+    vec3 refractVecB = refract(eyeVector, normal, iorRatioB);
+
+    color.r += texture2D(uTexture, uv + refractVecR.xy * (uRefractPower + slide * 1.0) * uChromaticAberration).r;
+    color.g += texture2D(uTexture, uv + refractVecG.xy * (uRefractPower + slide * 1.0) * uChromaticAberration).g;
+    color.b += texture2D(uTexture, uv + refractVecB.xy * (uRefractPower + slide * 1.0) * uChromaticAberration).b;
+
+    color = sat(color, uSaturation);
+  }
+
+  color /= float(LOOP);
+
+  gl_FragColor = vec4(color, 1.0);
   #include <tonemapping_fragment>
-#include <colorspace_fragment>
+  #include <colorspace_fragment>
 }
--- src/components/Geometries.tsx
+++ src/components/Geometries.tsx
@@ -6,6 +6,7 @@ import * as THREE from 'three';
 import { range } from '../../utils/render';
 import { useFBO } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
+import { folder, useControls } from 'leva';
 
 const Geometries = () => {
   const meshRef = useRef<THREE.Mesh>(null);
@@ -13,8 +14,33 @@ const Geometries = () => {
 
   const mainRenderTarget = useFBO();
 
+  const {
+    iorR,
+    iorG,
+    iorB,
+    saturation,
+    chromaticAberration,
+    refractPower,
+  } = useControls({
+    ior: folder({
+      title: 'IOR',
+      iorR: { value: 1.15, min: 1.0, max: 2.333, step: 0.001 },
+      iorG: { value: 1.18, min: 1.0, max: 2.333, step: 0.001 },
+      iorB: { value: 1.22, min: 1.0, max: 2.333, step: 0.001 },
+    }),
+    saturation: { value: 0.5, min: 0.0, max: 1.0, step: 0.01 },
+    chromaticAberration: { value: 0.5, min: 0.0, max: 1.5, step: 0.01 },
+    refractPower: { value: 1.06, min: 1, max: 1.25, step: 0.01 },
+  });
+
   const uniforms = useMemo(() => ({
     uTexture: { value: null },
+    uIorR: { value: 1.0 },
+    uIorG: { value: 1.0 },
+    uIorB: { value: 1.0 },
+    uSaturation: { value: 0.0 },
+    uChromaticAberration: { value: 1.0 },
+    uRefractPower: { value: 0.2 },
     winResolution: {
       value: new THREE.Vector2(
         window.innerWidth,
@@ -30,15 +56,27 @@ const Geometries = () => {
     gl.setRenderTarget(mainRenderTarget);
     gl.render(scene, camera);
 
-    meshRef.current.material.uniforms.uTexture.value = mainRenderTarget.texture;
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uTexture.value = mainRenderTarget.texture;
 
     gl.setRenderTarget(null);
     meshRef.current.visible = true;
+
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uIorR.value = iorR;
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uIorG.value = iorG;
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uIorB.value = iorB;
+
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uSaturation.value = saturation;
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uChromaticAberration.value = chromaticAberration;
+    (meshRef.current.material as THREE.ShaderMaterial).uniforms.uRefractPower.value = refractPower;
 
   });
 
   const columns = range(-7.5, 7.5, 2.5);
   const rows = range(-7.5, 7.5, 2.5);
+
+  // const randomColor = useMemo(() => {
+  //   return () => new THREE.Color(Math.random(), Math.random(), Math.random());
+  // }, []);
 
   return (
     <>
@@ -48,13 +86,13 @@ const Geometries = () => {
           rows.map((row, j) => (
             <mesh key={`${col}-${row}`} position={[col, row, -4]}>
               <icosahedronGeometry args={[0.333, 8]} />
-              <meshStandardMaterial color="#6366f1" />
+              <meshStandardMaterial color={'white'} />
             </mesh>
           ))
         )}
       </group>
       <mesh ref={meshRef}>
-        <icosahedronGeometry args={[2.84, 1]} />
+        <icosahedronGeometry args={[2.84, 20]} />
         <shaderMaterial
           vertexShader={vertexShader}
           fragmentShader={fragmentShader}
